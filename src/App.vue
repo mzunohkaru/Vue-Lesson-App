35a0c72ffd25d782cef8af1b231bb95422b1a52c
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-06-12 11:37:48 +0900

    study: basic vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+
+const price = ref(9.99)
+function increasePrice() {
+  price.value += 1
+}
+const info = ref({
+  name: 'Taro',
+  age: 20,
+  email: 'taro@example.com',
+})
+const info2 = reactive({
+  name: 'Ziro',
+  age: 20,
+  email: ref('taro@example.com'),
+  bio: 'hello',
+})
+const items = ref(['バナナ', 'apple', 'orange'])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <h1>Hello {{ info.name }}</h1>
+    <h1>{{ info2.bio }} {{ info2.name }}</h1>
+    <button @click="info.name = 'Jiro'">Change Name</button>
+    <p>Price: {{ price }}</p>
+    <button @click="increasePrice">Increase Price</button>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+    <div>
+      {{ price > 10 ? 'high' : 'low' }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: red;
 }
 </style>
