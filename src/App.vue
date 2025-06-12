debe4a3f0392ab51fe29422c633baa9961c05b3f
水野光琉 <mizuno.hikaru.01@gmail.com>
2025-06-12 21:01:17 +0900

    update:ui todo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <TodoPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TodoPage from './pages/TodoPage.vue'
+import { ref } from 'vue'
+
+const title = ref('Todo')
+</script>
+
+<style scoped>
+/* アプリケーション全体のスタイル */
+</style>
---
+++ src/pages/TodoPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todo-page">
+    <h1>My Task</h1>
+    <TaskList
+      :tasks="tasks"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TaskList from '../components/TaskList.vue'
+import { ref } from 'vue'
+
+// ビジネスエンティティの型定義
+type Task = {
+  readonly id: string
+  title: string
+  completed: boolean
+}
+
+const tasks = ref<Task[]>([
+  { id: '1', title: 'タスク1', completed: false },
+  { id: '2', title: 'タスク2', completed: true },
+  { id: '3', title: 'タスク3', completed: false },
+])
+
+</script>
+
+<style scoped></style>
